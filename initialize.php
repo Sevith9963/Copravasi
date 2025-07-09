3b3737788f9c0bd3a3a0debdb5d0efeed980025b
Sevith9963 <sevithkumar114@gmail.com>
2025-07-09 16:54:16 +0530

    commint

--- initialize.php
+++ initialize.php
@@ -1,20 +1,26 @@
 <?php
+// Absolutely NO whitespace before this tag
+
+// Developer default data (fallback)
 $dev_data = array(
     'id' => '-1',
     'firstname' => 'Developer',
     'lastname' => '',
     'username' => 'dev_oretnom',
-    'password' => '5da283a2d990e8d8512cf967df5bc0d0',
+    'password' => '5da283a2d990e8d8512cf967df5bc0d0', // Pre-hashed (MD5-like, but consider stronger hashing)
     'last_login' => '',
     'date_updated' => '',
     'date_added' => ''
 );
 
+// Define constants only if not already defined
 if (!defined('base_url')) define('base_url', 'http://localhost/Copravasi/');
 if (!defined('base_app')) define('base_app', str_replace('\\', '/', __DIR__) . '/');
 if (!defined('dev_data')) define('dev_data', $dev_data);
-if (!defined('DB_SERVER')) define('DB_SERVER', "127.0.0.1");
-if (!defined('DB_USERNAME')) define('DB_USERNAME', "root");
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', "");
-if (!defined('DB_NAME')) define('DB_NAME', "tourism_db");
+
+// Database connection parameters
+if (!defined('DB_SERVER')) define('DB_SERVER', '127.0.0.1');
+if (!defined('DB_USERNAME')) define('DB_USERNAME', 'root');
+if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '');
+if (!defined('DB_NAME')) define('DB_NAME', 'tourism_db');
 ?>
